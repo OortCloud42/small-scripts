ad41fe2ea95d7c37bcc185a11cc40b5fd97c3d9f
OortCloud42 <53982755+OortCloud42@users.noreply.github.com>
2022-08-10 12:29:38 +0200

    ;;;;;;

--- debian-repositories.sh
+++ debian-repositories.sh
@@ -6,6 +6,8 @@ apt-get --yes install debian-keyring
 apt-get --yes update
 apt-get --yes install fasttrack-archive-keyring
 
+echo /dev/null > /etc/apt/sources.list.d/debian-fasttrack.list
+
 printf '\n#Debian fasttrack & backports' >> /etc/apt/sources.list.d/debian-fasttrack.list
 printf '\ndeb https://fasttrack.debian.net/debian-fasttrack/ bullseye-fasttrack main contrib' >> /etc/apt/sources.list.d/debian-fasttrack.list
 printf '\ndeb http://deb.debian.org/debian bullseye-backports main' >> /etc/apt/sources.list.d/debian-fasttrack.list
